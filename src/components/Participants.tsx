aa081ca5f42340324aa1484b0035d4b098b50ad9
dayoung <dayoungkim9105@gmail.com>
2021-06-22 16:56:20 +0900

    feat(.): 각 인풋에 onChange 달았다

--- src/components/Participants.tsx
+++ src/components/Participants.tsx
@@ -3,24 +3,10 @@ import { people, horses } from 'App/datas';
 import { Title, Div, Line, Btn } from 'components/styles';
 import { GameContext } from 'App/context';
 
-type Type = {
-  horse: string;
-  person: string;
-};
-
 const Participants: React.FC = () => {
   const { state, setState, SpeedShuffle } = useContext(GameContext);
 
-  const betting =
-    ({ horse, person }: Type) =>
-    () => {
-      setState((prevState) => ({
-        ...prevState,
-        bettingPerson: person,
-        bettingHorse: horse,
-      }));
-    };
-
+  console.log(state);
   return (
     <>
       <Div>
@@ -32,10 +18,11 @@ const Participants: React.FC = () => {
               {person.name}
               <select
                 onChange={(e) => {
-                  betting({
-                    person: person.name,
-                    horse: e.target.value,
-                  });
+                  setState((prevState) => ({
+                    ...prevState,
+                    bettingPerson: person.name,
+                    bettingHorse: e.target.value,
+                  }));
                 }}
               >
                 <option value="">말을 선택하세요</option>
@@ -49,7 +36,16 @@ const Participants: React.FC = () => {
                   );
                 })}
               </select>
-              <input type="text" placeholder="금액을 입력하세요" required />
+              <input
+                placeholder="금액을 입력하세요"
+                onChange={(e) => {
+                  setState((prevState) => ({
+                    ...prevState,
+                    bettingMoney: Number(e.target.value),
+                  }));
+                }}
+                required
+              />
             </Line>
           );
         })}
